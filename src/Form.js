50b7f4c8fa21eb584335a191a99d9aaa669640a9
amandalocher <akent22000@gmail.com>
2024-10-17 17:14:30 -0400

    initial commit

---
+++ src/Form.js
@@ -0,0 +1,11 @@
+
+
+function Form() {
+  return (
+    <div className="App">
+      <h1>Form</h1>
+    </div>
+  );
+}
+
+export default Form;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import './index.css';
 import App from './App';
+import Home from "./Home"
+import Form from "./Form"
+import Details from "./Details"
 import reportWebVitals from './reportWebVitals';
 
+const router = createBrowserRouter([
+    {
+        path: "/home",
+        element: <Home />
+    },
+    {
+        path: "/form",
+        element: <Form />
+    },
+    {
+        path: "/Details",
+        element: <Details />
+    }
+]);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<RouterProvider router={router} />);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
